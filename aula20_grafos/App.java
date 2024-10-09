faf7d46aa503ad186ad164150aee00c44075c14c
leonardoheredia <leonardo@heredia.com.br>
2024-10-09 19:51:03 -0300

    Aula 20 - Grafo com Matriz Adjacencia

--- aula20_grafos/App.java
+++ aula20_grafos/App.java
@@ -3,6 +3,12 @@ package aula20_grafos;
 public class App {
     public static void main(String[] args) {
         Grafo g = new Grafo(5);
+        g.adicionarAresta(0, 4);
+        g.adicionarAresta(0, 3);
+        g.adicionarAresta(2, 3);
         g.imprimirMatriz();
+
+        System.out.println("");
+        System.out.println(g.toDot());
     }
 }
--- aula20_grafos/Grafo.java
+++ aula20_grafos/Grafo.java
@@ -10,7 +10,26 @@ public class Grafo {
         numArestas = 0;
         matrizAdjacencia = new boolean[this.numVertices][this.numVertices];
     }
+    public void adicionarAresta(int v, int w) {
+        matrizAdjacencia[v][w] = true;
+        matrizAdjacencia[w][v] = true;
+        numArestas++;
+    }
 
+    public String toDot() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(System.lineSeparator()).append("Graph {");
+        for (int v = 0; v < numVertices; v++) {
+            for (int w = v; w < numVertices; w++) {
+                if(matrizAdjacencia[v][w]) {
+                    sb.append(System.lineSeparator());
+                    sb.append(v).append("--").append(w);
+                }
+            }
+        }
+        sb.append(System.lineSeparator()).append("}");
+        return sb.toString();
+    }
     public void imprimirMatriz() {
         System.out.println("");
         System.out.printf("  ");
@@ -26,5 +45,9 @@ public class Grafo {
             }
         }
     }
+
+    //CORRIGIR O toDot para imprimir vertices isolados
+    //implementar o removerAresta(v, w)
+    //implementar o exsiteAresta(v, w)
 
 }
